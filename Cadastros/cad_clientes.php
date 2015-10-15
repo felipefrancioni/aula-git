c51657d405656bf4a49e68a175036ec11b34f9a7
Felipe Inacio <felipe@softdec.com>
2015-10-14 21:46:45 -0300

    Adicionado campos nos cadastros de clientes e produtos

--- Cadastros/cad_clientes.php
+++ Cadastros/cad_clientes.php
@@ -1,1 +1,3 @@
-Cadastro de clientes
+<h1>Cadastro de clientes</h1>
+<input id="nome_cliente" name="nome" style="width:352px;" maxlength="60" type="text" required="required">
+<input id="telefone_cliente" name="telefone" style="width:352px;" maxlength="15" type="text" required="required">
--- Cadastros/cad_produtos.php
+++ Cadastros/cad_produtos.php
@@ -1,1 +1,3 @@
-Cadastro de produtos
+<h1>Cadastro de produtos</h1>
+<input id="nome_produto" name="produto" style="width:352px;" maxlength="60" type="text" required="required">
+<input id="tipo_produto" name="tipo_produto" style="width:352px;" maxlength="20" type="text" required="required">
