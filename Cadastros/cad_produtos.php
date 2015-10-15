da7ae3ca43db49ef2f98b1e12e9ff5ee7671c20e
Felipe Inacio <felipe@softdec.com>
2015-10-14 22:01:23 -0300

    Corrigido o maxlenght do campo tipo_produto do cad_produtos

--- Cadastros/cad_produtos.php
+++ Cadastros/cad_produtos.php
@@ -1,3 +1,3 @@
 <h1>Cadastro de produtos</h1>
 <input id="nome_produto" name="produto" style="width:352px;" maxlength="60" type="text" required="required">
-<input id="tipo_produto" name="tipo_produto" style="width:352px;" maxlength="20" type="text" required="required">
+<input id="tipo_produto" name="tipo_produto" style="width:352px;" maxlength="40" type="text" required="required">
